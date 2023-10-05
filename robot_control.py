840348365b8c99d89c5ef9f4f5888022cd881eaf
Marlies Goes <marlies.goes@icloud.com>
2023-10-05 16:09:19 -0400

    add basic movement test

--- robot_control.py
+++ robot_control.py
@@ -2,4 +2,26 @@ from DexArm_API.pydexarm.pydexarm import Dexarm
 
 dexarm = Dexarm(port="/dev/tty.usbmodem308B335D34381")
 
-dexarm.go_home()
+try:
+    print('Go Home')
+    dexarm.go_home()
+
+    print('Move to cheese')
+    dexarm.move_to(300, 0, -20)
+    print('Pick up cheese')
+    dexarm.air_picker_pick()
+    print('Move to destination')
+    dexarm.move_to(300, 0, 40)
+    dexarm.move_to(250, 100, 40)
+    dexarm.move_to(250, 100, 0)
+    print('Let go')
+    dexarm.air_picker_place()
+
+    print('Go Home')
+    dexarm.go_home()
+
+finally:
+    print('Stop Pump')
+    dexarm.air_picker_stop()
+    print('Close')
+    dexarm.close()
