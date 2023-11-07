e7a85c7dcb729f826991216a650cc18f5bcc1e16
Marlies Goes <marlies.goes@icloud.com>
2023-11-07 12:39:37 -0500

    strip text

--- misc.py
+++ misc.py
@@ -1,10 +1,10 @@
 def print_robot(text):
     green_start = "\033[92m🤖: "
     reset = "\033[0m"
-    print(green_start + text + reset)
+    print(green_start + text.strip() + reset)
 
 
 def print_user(text):
     green_start = "\033[38;5;220m😎: "
     reset = "\033[0m"
-    print(green_start + text + reset)
+    print(green_start + text.strip() + reset)
