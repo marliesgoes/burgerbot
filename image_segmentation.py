1f9b6e0f52ff41fcf49cd199a8c3a0189bc5b5e1
Marlies Goes <marlies.goes@icloud.com>
2023-11-09 12:32:48 -0500

    update docstrings

--- image_segmentation.py
+++ image_segmentation.py
@@ -2,9 +2,6 @@ def get_camera_image():
     """
     Captures an image using the system's camera and returns it.
 
-    This function is assumed to interface with a camera device, capture a still image,
-    and return it in a format that is compatible with further image processing functions.
-
     Returns:
         image: An image object that can be processed by other functions. The exact format
                of this object will depend on the implementation details and the image
@@ -16,10 +13,6 @@ def get_camera_image():
 def find_center_of(ingredient, image):
     """
     Locates the center of a specified ingredient in the given image.
-
-    This function is designed to perform image segmentation and object recognition
-    within the provided image to find the specified ingredient. It then calculates
-    and returns the coordinates of the center of that ingredient.
 
     Parameters:
         ingredient (str): The name of the ingredient to locate in the image.
@@ -36,10 +29,6 @@ def camera_to_robot_coords(camera_coords):
     """
     Converts camera image space coordinates to robot arm's coordinate system.
 
-    Given a point in the image captured by the camera, this function converts that point
-    to a coordinate system that can be understood by the robot arm, allowing for accurate
-    positioning and movement.
-
     Parameters:
         camera_coords (tuple): A tuple of (x, y) coordinates in the camera's image space.
 
@@ -52,10 +41,6 @@ def camera_to_robot_coords(camera_coords):
 def move_item(pickup_coords, dropoff_coords):
     """
     Commands the robot to move an item from pickup coordinates to dropoff coordinates.
-
-    This function directs the robot arm to move to the pickup coordinates, secure the item,
-    and then move it to the specified dropoff coordinates. This may involve complex path
-    planning and collision avoidance depending on the implementation.
 
     Parameters:
         pickup_coords (tuple): The (x, y, z) coordinates where the robot will pick up the item.
