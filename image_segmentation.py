ade65e0ebfecfdec998e0bce1287f41a032ea1b6
Chen Wu (吴尘) <henrychenwu98@gmail.com>
2023-12-01 16:48:42 -0500

    Update detection code

--- image_segmentation.py
+++ image_segmentation.py
@@ -1,5 +1,6 @@
-from segmentation_utils import detect, groundingdino_model, sam_predictor, segment, draw_mask, transform_image
+from segmentation_utils import detect, groundingdino_model, sam_predictor, segment, draw_mask, transform_image, annotate
 import PIL
+from PIL import Image
 import numpy as np
 
 
@@ -15,7 +16,7 @@ def get_camera_image():
     pass
 
 
-def find_center_of(ingredient, image, max_num=1, visualize=False):
+def find_center_of(ingredient, image, max_num=1, visualize=False, detection_only=True):
     """
     Locates the center of a specified ingredient in the given image.
 
@@ -27,29 +28,47 @@ def find_center_of(ingredient, image, max_num=1, visualize=False):
         list[tuple]: A list of tuples of (x, y) coordinates representing the central point(s) of 
         the detected instances of the ingredient in the image space.
     """
+    # Flip the image
+    image = image.transpose(PIL.Image.FLIP_LEFT_RIGHT)
     image_source, image = transform_image(image)
-    detected_boxes = detect(image, text_prompt=ingredient, model=groundingdino_model)
+    detected_boxes, logits, phrases = detect(image, text_prompt=ingredient, model=groundingdino_model)
     print(f"Detected {len(detected_boxes)} instances of {ingredient}, picking the top {max_num}...")
     detected_boxes = detected_boxes[:max_num]
-    segmented_frame_masks = segment(image_source, sam_predictor, boxes=detected_boxes)
+    print("detected_boxes:", detected_boxes)
+    if not detection_only:
+        segmented_frame_masks = segment(image_source, sam_predictor, boxes=detected_boxes)
     
     if visualize:
-        print("Visualizing the top segmentation mask")
-        annotated_frame_with_mask = draw_mask(segmented_frame_masks[0][0], image_source)
-        # Visualize the annotated frame with mask
-        image = PIL.Image.fromarray(annotated_frame_with_mask)
-        image.show()
+        if not detection_only:
+            print("Visualizing the top segmentation mask")
+            annotated_frame_with_mask = draw_mask(segmented_frame_masks[0][0], image_source)
+            # Visualize the annotated frame with mask
+            image = PIL.Image.fromarray(annotated_frame_with_mask)
+            image.show()
+        else:
+            # Visualize the detected boxes
+            annotated_frame = annotate(image_source=image_source, boxes=detected_boxes, logits=logits, phrases=phrases)
+            annotated_frame = annotated_frame[...,::-1] # BGR to RGB
+            annotated_image = Image.fromarray(annotated_frame)
+            annotated_image.show()
     
     # Find the center of the masks
     centers = []
-    for mask in segmented_frame_masks:
-        mask = mask[0]
-        mask = mask.cpu().numpy()
-        pixels = np.argwhere(mask > 0)
-        center = np.mean(pixels, axis=0)
-        y, x = center
-        centers.append((int(x), int(y)))
-    
+    if not detection_only:
+        for mask in segmented_frame_masks:
+            mask = mask[0]
+            mask = mask.cpu().numpy()
+            pixels = np.argwhere(mask > 0)
+            center = np.mean(pixels, axis=0)
+            y, x = center
+            centers.append((int(x), int(y)))
+    else:
+        for detected_box in detected_boxes:
+            x1, y1, w, h = detected_box
+            center = x1, y1
+            center = (int(center[0] * image_source.shape[1]), int(center[1] * image_source.shape[0]))
+            centers.append(center)
+
     return centers
 
 
@@ -83,13 +102,13 @@ def move_item(pickup_coords, dropoff_coords):
 # Test code
 if __name__ == "__main__":
     import PIL
-    image = PIL.Image.open("caliResulttr2.png")
+    image = PIL.Image.open("cheese_a.jpg")
     for ingredient, max_num in [
-        ("green lettuce toy", 1),
-        ("red tomato toy", 1),
+        # ("green lettuce toy", 1),
+        # ("red tomato toy", 1),
         ("yellow cheese toy", 1),
-        ("brown meat toy", 1),
-        ("almond-color bun toy", 2),
+        # ("brown meat toy", 1),
+        # ("almond-color bun toy", 2),
     ]:
-        ingredient_centers = find_center_of(ingredient, image, max_num=max_num)
+        ingredient_centers = find_center_of(ingredient, image, max_num=max_num, visualize=False)
         print(f"{ingredient} centers:", ingredient_centers)
