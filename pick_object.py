3a2e2e3ef961a9878a4b56acd30145aad864d147
Marlies Goes <marlies.goes@icloud.com>
2023-12-06 13:09:22 -0500

    make moving smoother

--- pick_object.py
+++ pick_object.py
@@ -1,6 +1,7 @@
 from DexArm_API.pydexarm.pydexarm import Dexarm
 import numpy as np
 import pandas as pd
+import time
 
 df = pd.read_csv('calib_coords.csv')
 
@@ -22,28 +23,46 @@ def camera_to_robot_coords(cam_x, cam_y):
     return int(rob_x), int(rob_y)
 
 
-def move_item(robo_coor, dropoff_coords=(300, 100), dropoff_height=-55, pickup_height=-55):
+def move_items(found_items, recipe):
     """
     Commands the robot to move an item from pickup coordinates to dropoff coordinates.
 
     Parameters:
-        pickup_coords (tuple): The (x, y, z) coordinates where the robot will pick up the item.
-        dropoff_coords (tuple): The (x, y, z) coordinates where the robot will place the item.
+        found_items (dict): Dictionary of items with their corresponding coordinates.
+        recipe (list): List of items to be moved.
 
     Returns:
-        bool: True if the item was successfully moved, False otherwise.
+        bool: True if all items were successfully moved, False otherwise.
     """
+    dropoff_coords = (300, 100)
+    pickup_height = -65
+
     dexarm = Dexarm(port="/dev/tty.usbmodem308B335D34381")
     dexarm.go_home()
-    dexarm.move_to(robo_coor[0], robo_coor[1], 0)
-    dexarm.move_to(robo_coor[0], robo_coor[1], pickup_height)
-    dexarm.air_picker_pick()
-    dexarm.move_to(robo_coor[0], robo_coor[1], 0)
+    i = 0
 
-    dexarm.move_to(dropoff_coords[0], dropoff_coords[1], 0)
-    dexarm.move_to(dropoff_coords[0], dropoff_coords[1], dropoff_height)
+    try:
+        for item in recipe:
+            robo_coor = found_items[item].pop()
+            dropoff_height = -55 + (i * 10)
+            dexarm.move_to(robo_coor[0], robo_coor[1], 0)
+            dexarm.move_to(robo_coor[0], robo_coor[1], pickup_height)
+            dexarm.air_picker_pick()
+            time.sleep(1)
+            dexarm.move_to(robo_coor[0], robo_coor[1], 0)
 
-    dexarm.air_picker_place()
-    dexarm.move_to(dropoff_coords[0], dropoff_coords[1], 0)
-    dexarm.go_home()
-    dexarm.air_picker_stop()
+            dexarm.move_to(dropoff_coords[0], dropoff_coords[1], 0)
+            dexarm.move_to(dropoff_coords[0],
+                           dropoff_coords[1], dropoff_height)
+
+            dexarm.air_picker_place()
+            dexarm.move_to(dropoff_coords[0], dropoff_coords[1], 0)
+            i += 1
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
+    finally:
+        dexarm.go_home()
+        dexarm.air_picker_stop()
+
+    return True
